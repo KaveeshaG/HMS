65241826238693a1a06dd74b6c205875f94a0b88
KaveeshaG <uakaveeshagimhana@gmail.com>
2020-04-19 22:12:01 +0530

    Project Updated

---
+++ HospitalService/src/com/hcs/service/HospitalService.java
@@ -0,0 +1,78 @@
+package com.hcs.service;
+
+import java.text.ParseException;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+import com.hcs.controller.HospitalController;
+import com.hcs.model.Hospital;
+
+//For XML
+
+@Path("/Hospital")
+public class HospitalService {
+
+	HospitalController hospitalController = new HospitalController();
+
+	@GET
+	@Path("/read")
+	@Produces({ MediaType.TEXT_PLAIN })
+	public String readHospital() {
+		return new Gson().toJson(hospitalController.readHospital());
+	}
+
+	@POST
+	@Path("/insert")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertHospital(@FormParam("hospitalname") String hospitalname, @FormParam("hospitaladdress") String hospitaladdress,
+			@FormParam("contactnumber") String contactnumber, @FormParam("email") String email) throws ParseException {
+
+		Hospital hospital = new Hospital();
+		hospital.setHospitalname(hospitalname);
+		hospital.setHospitaladdress(hospitaladdress);
+		hospital.setContactnumber(contactnumber);
+		hospital.setEmail(email);
+
+		return hospitalController.Addhospital(hospital);
+	}
+
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteHospital(@PathParam("id")String hid) {
+
+		return hospitalController.deleteHospital(hid);
+	}
+	
+	
+	@PUT
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateHospital(@FormParam("hid") String hid,@FormParam("hospitalname") String hospitalname, @FormParam("hospitaladdress") String hospitaladdress,
+			@FormParam("contactnumber") String contactnumber, @FormParam("email") String email) throws ParseException {
+
+		Hospital hospital = new Hospital();
+		hospital.setHid(Integer.parseInt(hid));
+		hospital.setHospitalname(hospitalname);
+		hospital.setHospitaladdress(hospitaladdress);
+		hospital.setContactnumber(contactnumber);
+		hospital.setEmail(email);
+
+		return hospitalController.updatehospital(hospital);
+	}
+	
+	@GET
+	@Path("/search/{id}")
+	@Produces({ MediaType.TEXT_PLAIN })
+	public String searchHospital(@PathParam("id")String hid) {
+		return new Gson().toJson(hospitalController.searchHospital(hid));
+	}
+	
+	
+
+}
